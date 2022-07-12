5829db73b3c1c95948b9e91fbbb9edd79b258769
mahmoud mohamed <newcairo06@gmail.com>
2022-07-12 19:00:46 +0200

    inital commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/*var Name=50;
+console.log(Name);
+const Pi=525;
+Pi=777;
+console.log(Pi);*/
+/*var N1=10;
+N1+=100;
+console.log(N1);*/
+/*var N1="mahmoud";
+function say(){
+console.log("hello",N1);
+}
+say();*/
+/*var cars=["bmw","byd","toyota"];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+cars.push("reno");
+cars.pop();
+console.log(cars[3]);*/
+var N1=[];
+/*if(N1==50){
+  document.write("true");
+}
+else{
+  document.write("false");
+}*/
+/*switch(N1){
+  case 84:
+    console.log("non");
+    break;
+    case 50:
+      console.log("ok")
+      break;
+      default :
+      console.log("not macth")
+}*/
+//var m ='hot price';
+var i=0;
+while(i<5){
+  N1.push(i);
+  i++;
+}
+document.write(N1);
